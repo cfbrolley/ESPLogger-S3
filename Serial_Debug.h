d05581be0b935903151d77e7192c7f64b890885e
cfbrolley <119771818+cfbrolley@users.noreply.github.com>
2024-08-18 17:37:47 +1000

    Update Serial_Debug.h

--- Serial_Debug.h
+++ Serial_Debug.h
@@ -9,7 +9,7 @@ class Serial_Debug
     Serial_Debug(int baud); //pass baud rate for serial monitor
     void begin();
     void debugdata(unsigned long _timer, float _pressure, float _altitude, float _correctedalt, float _ax, float _ay, float _az, float _gx, float _gy, float _gz, int _state); //pass all logger variables
-    void debugBMP(int _BMPcode, float  _altioffset); //allows passing of an identifier to display different error messages
+    void debugBMP(int _BMPcode, float  _altioffset); //allows passing of an error code/identifier to display different error messages
     void debugIMU(int _IMUcode);
     void debugSD(int _SDcode);
     void debuggeneric();
